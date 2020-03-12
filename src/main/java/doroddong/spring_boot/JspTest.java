d2436980dea929b47c1c9d90f6204f494f7037f9
doroddong <dongpang1014@ajou.ac.kr>
2020-03-13 00:14:41 +0900

    Connect JSP

---
+++ src/main/java/doroddong/spring_boot/JspTest.java
@@ -0,0 +1,13 @@
+package doroddong.spring_boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JspTest {
+
+    @RequestMapping("/test")
+    private String jspTest(){
+        return "test"; //생성한 jsp명 (test.jsp)
+    }
+}
