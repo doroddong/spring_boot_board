6f63019fdbcd898e24ef0228fc971f36b8c8f2fd
doroddong <dongpang1014@ajou.ac.kr>
2020-03-13 00:43:10 +0900

    Connect MySQL,MyBaties

--- src/main/java/doroddong/spring_boot/JspTest.java
+++ src/main/java/doroddong/spring_boot/JspTest.java
@@ -1,10 +1,16 @@
 package doroddong.spring_boot;
 
+import doroddong.spring_boot.board.mapper.BoardMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
+
 @Controller
 public class JspTest {
+
+    @Resource(name="doroddong.spring_boot.board.mapper.BoardMapper")
+    BoardMapper mBoardMapper;
 
     @RequestMapping("/test")
     private String jspTest(){
